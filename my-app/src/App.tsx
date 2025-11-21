03fc6108c99f81c532eadca2927cbc15b3e90280
Kaja <kjc@spyro-soft.com>
2025-11-21 10:11:36 +0100

    wersja ostateczna na teraz

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,6 +4,10 @@ import HomePage from './components/HomePage';
 import RandomGenerator from './components/RandomGenerator';
 import ToDo from './components/ToDo';
 import Quiz from './components/Quiz';
+import Game from './components/GamePage';
+import Expenses from './components/Expences';
+import Tshirts from './components/Tshirts';
+import XO from './components/XO';
 
 const App: React.FC = () => {
   return (
@@ -13,6 +17,10 @@ const App: React.FC = () => {
         <Route path="/random-generator" element={<RandomGenerator />} />
         <Route path="/toDo-List" element={<ToDo />} />
         <Route path="/Quiz" element={<Quiz />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/XO" element={<XO />} />
+        <Route path="/expenses" element={<Expenses />} />
+        <Route path="/t-shirts" element={<Tshirts />} />
       </Routes>
     </Router>
   );
--- my-app/src/components/Quiz.tsx
+++ my-app/src/components/Quiz.tsx
@@ -57,9 +57,12 @@ const Quiz: React.FC = () => {
       <nav>
         <ul className='nav-links'>
           <li className='links'><Link to="/">Home</Link></li>
-          <li className='links'><Link to="/toDo-List">To do list</Link></li>
-          <li className='links'><a href="services.html">Services</a></li>
-          <li className='links'><a href="/Quiz">Quiz</a></li>
+          <li className='links'><Link to="/ToDo-List">To Do List</Link></li>
+          <li className='links'><Link to="/Quiz">Quiz</Link></li>
+          <li className='links'><Link to="/game">Games reservation</Link></li>
+          <li className='links'><Link to="/XO">XO Game</Link></li>
+          <li className='links'><Link to="/expenses">Expenses Tracker</Link></li>
+          <li className='links'><Link to="/t-shirts">Koszulki firmowe</Link></li>
         </ul>
       </nav>
 
